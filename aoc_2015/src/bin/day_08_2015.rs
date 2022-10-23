50f93530ee3402b69ea8dba906d225431d38147d
JeanpecheGH <gildasdesnos@hotmail.com>
2022-10-23 20:22:19 +0200

    Add 2016 - Day 08

---
+++ aoc_2015/src/bin/day_08_2015.rs
@@ -0,0 +1,42 @@
+use util;
+
+fn main() {
+    let lines = util::file_as_lines("aoc_2015/input/day_08.txt").expect("Cannot open input file");
+
+    let part1_diff: Option<(u16, u16)> = lines
+        .map(|l| {
+            let line = l.unwrap();
+            let current_size = line.len() as u16;
+            (
+                current_size - memory_size(&line, 0) + 2,
+                encoded_size(&line) - current_size,
+            )
+        })
+        .reduce(|a, b| (a.0 + b.0, a.1 + b.1));
+
+    println!("Part1: The difference is {}", part1_diff.unwrap().0);
+    println!("Part2: The difference is now {}", part1_diff.unwrap().1);
+}
+
+fn memory_size(s: &str, acc: u16) -> u16 {
+    match s.len() {
+        0 => acc,
+        x if x > 1 => match (s.chars().nth(0), s.chars().nth(1)) {
+            (Some('\\'), Some('\\')) => memory_size(&s[2..], acc + 1),
+            (Some('\\'), Some('\"')) => memory_size(&s[2..], acc + 1),
+            (Some('\\'), Some('x')) => memory_size(&s[4..], acc + 1),
+            _ => memory_size(&s[1..], acc + 1),
+        },
+        _ => memory_size(&s[1..], acc + 1),
+    }
+}
+
+fn encoded_size(s: &str) -> u16 {
+    s.chars()
+        .map(|c| match c {
+            '\\' | '\"' => 2,
+            _ => 1,
+        })
+        .sum::<u16>()
+        + 2
+}
