20b0a666791d24bcfa16e9f354506674398bb0c9
JeanpecheGH <gildasdesnos@hotmail.com>
2022-10-26 14:09:55 +0200

    Add 2015 - Day 15 & 16

---
+++ aoc_2015/src/bin/day_15_2015.rs
@@ -0,0 +1,114 @@
+struct Ingredient {
+    capacity: i64,
+    durability: i64,
+    flavor: i64,
+    texture: i64,
+    calories: i64,
+}
+
+impl Ingredient {
+    fn mult(&self, fact: i64) -> Self {
+        Ingredient {
+            capacity: self.capacity * fact,
+            durability: self.durability * fact,
+            flavor: self.flavor * fact,
+            texture: self.texture * fact,
+            calories: self.calories * fact,
+        }
+    }
+    fn add(&self, other: Ingredient) -> Self {
+        Ingredient {
+            capacity: self.capacity + other.capacity,
+            durability: self.durability + other.durability,
+            flavor: self.flavor + other.flavor,
+            texture: self.texture + other.texture,
+            calories: self.calories + other.calories,
+        }
+    }
+    fn score(&self, with_calories: bool) -> i64 {
+        if with_calories {
+            return self.score_with_calories();
+        }
+        match (self.capacity, self.durability, self.flavor, self.texture) {
+            (i64::MIN..=0, _, _, _) => 0,
+            (_, i64::MIN..=0, _, _) => 0,
+            (_, _, i64::MIN..=0, _) => 0,
+            (_, _, _, i64::MIN..=0) => 0,
+            (a, b, c, d) => a * b * c * d,
+        }
+    }
+    fn score_with_calories(&self) -> i64 {
+        match (
+            self.calories,
+            self.capacity,
+            self.durability,
+            self.flavor,
+            self.texture,
+        ) {
+            (_, i64::MIN..=0, _, _, _) => 0,
+            (_, _, i64::MIN..=0, _, _) => 0,
+            (_, _, _, i64::MIN..=0, _) => 0,
+            (_, _, _, _, i64::MIN..=0) => 0,
+            (500, a, b, c, d) => a * b * c * d,
+            _ => 0,
+        }
+    }
+}
+
+fn main() {
+    let lines = util::file_as_lines("aoc_2015/input/day_15.txt").expect("Cannot open input file");
+
+    let ingredients: Vec<Ingredient> = lines
+        .map(|l| {
+            let s: String = l.unwrap();
+            let words: Vec<&str> = s.split(' ').collect();
+
+            let capacity: i64 = words[2].strip_suffix(',').unwrap().parse::<i64>().unwrap();
+            let durability: i64 = words[4].strip_suffix(',').unwrap().parse::<i64>().unwrap();
+            let flavor: i64 = words[6].strip_suffix(',').unwrap().parse::<i64>().unwrap();
+            let texture: i64 = words[8].strip_suffix(',').unwrap().parse::<i64>().unwrap();
+            let calories: i64 = words[10].parse().unwrap();
+            Ingredient {
+                capacity,
+                durability,
+                flavor,
+                texture,
+                calories,
+            }
+        })
+        .collect();
+
+    let res = cookie_scores(ingredients.as_slice(), 100, false);
+    println!("Part1: Max cookie score is {}", res);
+    let res_2 = cookie_scores(ingredients.as_slice(), 100, true);
+    println!(
+        "Part2: Max cookie score is {} with a limit of 500 calories",
+        res_2
+    );
+}
+
+fn cookie_scores(ingredients: &[Ingredient], total_spoons: i64, with_calories: bool) -> i64 {
+    let mut max_score = 0;
+    for i in 0..=total_spoons {
+        for j in 0..=(total_spoons - i) {
+            for k in 0..=(total_spoons - i - j) {
+                let l = total_spoons - i - j - k;
+                let score = score(ingredients, vec![i, j, k, l], with_calories);
+                if score > max_score {
+                    max_score = score;
+                }
+            }
+        }
+    }
+    max_score
+}
+
+fn score(ingredients: &[Ingredient], spoons: Vec<i64>, with_calories: bool) -> i64 {
+    ingredients
+        .iter()
+        .zip(spoons)
+        .map(|(ing, spoon)| ing.mult(spoon))
+        .reduce(|a, b| a.add(b))
+        .map(|ing| ing.score(with_calories))
+        .unwrap()
+}
---
+++ aoc_2015/src/bin/day_16_2015.rs
@@ -0,0 +1,85 @@
+use std::collections::HashMap;
+
+#[derive(Clone, Debug)]
+struct AuntSue {
+    id: u32,
+    attributes: HashMap<String, u32>,
+}
+
+impl AuntSue {
+    fn is_valid(&self, other: &AuntSue) -> bool {
+        self.attributes
+            .iter()
+            .all(|(attr, &val)| other.attributes[attr] == val)
+    }
+    fn is_valid_2(&self, other: &AuntSue) -> bool {
+        self.attributes
+            .iter()
+            .all(|(attr, &val)| match attr.as_str() {
+                "cats" | "trees" => other.attributes[attr] < val,
+                "pomeranians" | "goldfish" => other.attributes[attr] > val,
+                _ => other.attributes[attr] == val,
+            })
+    }
+}
+
+fn main() {
+    let lines = util::file_as_lines("aoc_2015/input/day_16.txt").expect("Cannot open input file");
+
+    let aunts: Vec<AuntSue> = lines
+        .map(|l| {
+            let s: String = l.unwrap();
+            let words: Vec<&str> = s.split(' ').collect();
+
+            let id: u32 = words[1].strip_suffix(':').unwrap().parse::<u32>().unwrap();
+            let att_1: &str = words[2].strip_suffix(':').unwrap();
+            let val_1: u32 = words[3].strip_suffix(',').unwrap().parse::<u32>().unwrap();
+            let att_2: &str = words[4].strip_suffix(':').unwrap();
+            let val_2: u32 = words[5].strip_suffix(',').unwrap().parse::<u32>().unwrap();
+            let att_3: &str = words[6].strip_suffix(':').unwrap();
+            let val_3: u32 = words[7].parse::<u32>().unwrap();
+
+            let mut attributes = HashMap::new();
+            attributes.insert(att_1.to_string(), val_1);
+            attributes.insert(att_2.to_string(), val_2);
+            attributes.insert(att_3.to_string(), val_3);
+
+            AuntSue { id, attributes }
+        })
+        .collect();
+    let mut target_sue_attributes = HashMap::new();
+    target_sue_attributes.insert("children".to_string(), 3);
+    target_sue_attributes.insert("cats".to_string(), 7);
+    target_sue_attributes.insert("samoyeds".to_string(), 2);
+    target_sue_attributes.insert("pomeranians".to_string(), 3);
+    target_sue_attributes.insert("akitas".to_string(), 0);
+    target_sue_attributes.insert("vizslas".to_string(), 0);
+    target_sue_attributes.insert("goldfish".to_string(), 5);
+    target_sue_attributes.insert("trees".to_string(), 3);
+    target_sue_attributes.insert("cars".to_string(), 2);
+    target_sue_attributes.insert("perfumes".to_string(), 1);
+    let target_sue = AuntSue {
+        id: 0,
+        attributes: target_sue_attributes,
+    };
+
+    let mut valid_aunts: Vec<AuntSue> = aunts
+        .iter()
+        .filter(|&aunt| aunt.is_valid(&target_sue))
+        .cloned()
+        .collect();
+    println!(
+        "Part1: The good Sue is number {:?}",
+        valid_aunts.pop().unwrap().id
+    );
+
+    let mut valid_aunts_2: Vec<AuntSue> = aunts
+        .iter()
+        .filter(|&aunt| aunt.is_valid_2(&target_sue))
+        .cloned()
+        .collect();
+    println!(
+        "Part2: The good Sue is finally number {:?}",
+        valid_aunts_2.pop().unwrap().id
+    );
+}
