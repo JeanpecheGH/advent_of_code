84c79e062d61da9ad058b9d97b9de5de8dc29bfb
JeanpecheGH <gildasdesnos@hotmail.com>
2022-11-04 18:49:48 +0100

    Add 2015 - Day 24

---
+++ aoc_2015/src/bin/day_24_2015.rs
@@ -0,0 +1,46 @@
+fn main() {
+    let lines = util::file_as_lines("aoc_2015/input/day_24.txt").expect("Cannot open input file");
+
+    let packages: Vec<u64> = lines.map(|l| l.unwrap().parse().unwrap()).collect();
+
+    let weight_sum: u64 = packages.iter().sum();
+
+    let res1 = fill_packages(packages.clone(), weight_sum / 3, 6, Vec::new());
+    let mut qes1: Vec<u64> = res1.iter().map(|v| v.iter().product()).collect();
+    qes1.sort_unstable();
+    println!(
+        "Part1: The lowest Quantum Entanglement whe dividing in 3 groups is {:?}",
+        qes1.first().unwrap()
+    );
+
+    let res2 = fill_packages(packages, weight_sum / 4, 4, Vec::new());
+    let mut qes2: Vec<u64> = res2.iter().map(|v| v.iter().product()).collect();
+    qes2.sort_unstable();
+    println!(
+        "Part2: The lowest Quantum Entanglement whe dividing in 4 groups is {:?}",
+        qes2.first().unwrap()
+    );
+}
+
+fn fill_packages(
+    mut packages: Vec<u64>,
+    target: u64,
+    max_packages: usize,
+    current_set: Vec<u64>,
+) -> Vec<Vec<u64>> {
+    if current_set.len() > max_packages {
+        return Vec::new();
+    }
+    match (packages.pop(), target) {
+        (Some(p), t) if t >= p => {
+            let mut new_set = current_set.clone();
+            new_set.push(p);
+            let mut resp = fill_packages(packages.clone(), t - p, max_packages, new_set);
+            resp.extend(fill_packages(packages, t, max_packages, current_set));
+            resp
+        }
+        (Some(_), t) => fill_packages(packages, t, max_packages, current_set),
+        (None, 0) => vec![current_set],
+        _ => Vec::new(),
+    }
+}
