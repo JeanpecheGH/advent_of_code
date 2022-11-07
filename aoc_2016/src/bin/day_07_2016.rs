f9e1dab4c479e737ddbf45158d7b7c673a89a828
JeanpecheGH <gildasdesnos@hotmail.com>
2022-11-07 19:06:01 +0100

    Add 2016 - Day 07

---
+++ aoc_2016/src/bin/day_07_2016.rs
@@ -0,0 +1,78 @@
+struct IPv7 {
+    supernets: Vec<String>,
+    hypernets: Vec<String>,
+}
+
+impl IPv7 {
+    fn support_tls(&self) -> bool {
+        self.supernets.iter().any(|spn| IPv7::has_abba(spn))
+            && self.hypernets.iter().all(|hpn| !IPv7::has_abba(hpn))
+    }
+
+    fn support_ssl(&self) -> bool {
+        let supernet_abas = IPv7::get_abas(&self.supernets);
+        let hypernet_abas = IPv7::get_abas(&self.hypernets);
+
+        supernet_abas
+            .iter()
+            .any(|&spn| hypernet_abas.iter().any(|&hpn| spn[..2] == hpn[1..]))
+    }
+
+    fn has_abba(net: &str) -> bool {
+        let chars: Vec<char> = net.chars().collect();
+        chars
+            .windows(4)
+            .any(|win| win[0] == win[3] && win[1] == win[2] && win[0] != win[1])
+    }
+
+    fn get_abas(sequences: &[String]) -> Vec<&str> {
+        sequences
+            .iter()
+            .flat_map(|sqn| {
+                sqn.as_bytes()
+                    .windows(3)
+                    .enumerate()
+                    .filter_map(|(index, win)| {
+                        //We compare u8, but we return &str
+                        if win[0] == win[2] && win[0] != win[1] {
+                            Some(&sqn[index..=index + 2])
+                        } else {
+                            None
+                        }
+                    })
+            })
+            .collect()
+    }
+}
+
+fn main() {
+    let lines = util::file_as_lines("aoc_2016/input/day_07.txt").expect("Cannot open input file");
+
+    let ips: Vec<IPv7> = lines
+        .map(|l| {
+            let s = l.unwrap();
+            let words: Vec<&str> = s.split(|c| c == ']' || c == '[').collect();
+            let mut supernets: Vec<String> = Vec::new();
+            let mut hypernets: Vec<String> = Vec::new();
+            for (index, &sequence) in words.iter().enumerate() {
+                if index % 2 == 0 {
+                    supernets.push(sequence.to_string());
+                } else {
+                    hypernets.push(sequence.to_string());
+                }
+            }
+            IPv7 {
+                supernets,
+                hypernets,
+            }
+        })
+        .collect();
+
+    let nb_tls: usize = ips.iter().filter(|&ip| ip.support_tls()).count();
+
+    println!("Part1: {} ips support TLS", nb_tls);
+
+    let nb_ssl: usize = ips.iter().filter(|&ip| ip.support_ssl()).count();
+
+    println!("Part1: {} ips support SSL", nb_ssl);
+}
