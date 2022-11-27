4a50d820c5f8303f3ed531e6675085a00ff236be
JeanpecheGH <gildasdesnos@hotmail.com>
2022-11-27 12:48:21 +0100

    Improve 2016 day 16 (remove use of a storing String)

--- aoc_2016/src/bin/day_16_2016.rs
+++ aoc_2016/src/bin/day_16_2016.rs
@@ -1,49 +1,116 @@
+use std::cmp::{max, min};
+
 fn main() {
     let input: &str = "11100010111110100";
-    let target_size_part1: usize = 272;
 
     println!("Input: {}", input);
+    println!("Reverse: {}", reverse(input));
+
     let now = std::time::Instant::now();
-    let dr: String = dragon_string(input, target_size_part1);
-    let cs: String = checksum(&dr);
-    let elapsed = now.elapsed();
+    //Part1 optimized
+    let target_size: usize = 272;
+    let mut chunk_size = 1;
+    while target_size % (chunk_size * 2) == 0 {
+        chunk_size *= 2;
+    }
+    let cs: String = (0..target_size / chunk_size)
+        .map(|i| checksum_chunk(input, i * chunk_size, chunk_size))
+        .collect();
     println!(
         "Part1: The checksum for target size {} is {}, found in {:?}",
-        cs, target_size_part1, elapsed
+        target_size,
+        cs,
+        now.elapsed()
     );
-
-    let target_size_part2: usize = 35651584;
 
     let now = std::time::Instant::now();
-    let dr: String = dragon_string(input, target_size_part2);
-    let cs: String = checksum(&dr);
-    let elapsed = now.elapsed();
+    //Part2 optimized
+    let target_size: usize = 35651584;
+    let mut chunk_size = 1;
+    while target_size % (chunk_size * 2) == 0 {
+        chunk_size *= 2;
+    }
+    let cs: String = (0..target_size / chunk_size)
+        .map(|i| checksum_chunk(input, i * chunk_size, chunk_size))
+        .collect();
     println!(
         "Part2: The checksum for target size {} is {}, found in {:?}",
-        cs, target_size_part2, elapsed
+        target_size,
+        cs,
+        now.elapsed()
     );
 }
 
-fn checksum(input: &str) -> String {
-    let chars: Vec<char> = input.chars().collect();
-    let s: String = chars
-        .chunks(2)
-        .map(|pair| if pair[0] == pair[1] { '1' } else { '0' })
-        .collect();
-    if s.len() % 2 == 0 {
-        checksum(&s)
+fn checksum_chunk(input: &str, chunk_start: usize, chunk_size: usize) -> char {
+    let len: usize = input.len() + 1;
+    let rev: String = reverse(input);
+    let chunk_end: usize = chunk_start + chunk_size;
+    let start_pairs: usize = if chunk_start % (len * 2) == 0 {
+        chunk_start
     } else {
-        s
+        chunk_start + len * 2 - chunk_start % (len * 2)
+    };
+    let end_pairs: usize = max(chunk_end - chunk_end % (len * 2), start_pairs);
+    let offset: usize = if end_pairs > (chunk_end + 1) {
+        end_pairs - (chunk_end + 1)
+    } else {
+        0
+    };
+    //Count nb of 1 in prefix
+    let nb_prefix = affix_ones(
+        input,
+        &rev,
+        min(start_pairs as isize - 1, chunk_end as isize) - chunk_start as isize,
+        offset,
+        true,
+        len,
+    );
+    //Count nb of input/rev pairs (this parity is the same as the number of ones in all pairs combined)
+    let nb_pairs = (end_pairs - start_pairs) / (len * 2) * (len - 1);
+    //Count nb of 1 in delimiters
+    let nb_delim = delimiters_ones(chunk_start, chunk_end, len);
+    //Count nb of 1 in suffix
+    let suffix_size: usize = if chunk_end >= end_pairs {
+        chunk_end - end_pairs
+    } else {
+        0
+    };
+    let nb_suffix = affix_ones(input, &rev, suffix_size as isize, 0, false, len);
+
+    if (nb_prefix + nb_pairs + nb_delim + nb_suffix) % 2 == 0 {
+        '1'
+    } else {
+        '0'
     }
 }
 
-fn dragon_string(input: &str, target_size: usize) -> String {
-    let s: String = format!("{}0{}", input, reverse(input));
-    if s.len() >= target_size {
-        s[..target_size].to_owned()
-    } else {
-        dragon_string(&s, target_size)
+fn affix_ones(
+    input: &str,
+    rev: &str,
+    size: isize,
+    offset: usize,
+    from_end: bool,
+    len: usize,
+) -> usize {
+    if size <= 0 {
+        return 0;
     }
+    let actual_size = if (size as usize + offset) >= len {
+        size as usize - 1
+    } else {
+        size as usize
+    };
+    let affixes = format!("{}{}", input, rev);
+    let mut chars: Vec<char> = affixes.chars().collect();
+    if from_end {
+        chars = chars.into_iter().rev().collect();
+    }
+    chars
+        .into_iter()
+        .skip(offset)
+        .take(actual_size)
+        .filter(|&c| c == '1')
+        .count()
 }
 
 fn reverse(input: &str) -> String {
@@ -56,4 +123,22 @@ fn reverse(input: &str) -> String {
             _ => ' ',
         })
         .collect()
+}
+
+fn delimiters_ones(start: usize, end: usize, div: usize) -> usize {
+    let s = if (start + 1) % div == 0 {
+        (start + 1) / div
+    } else {
+        ((start + 1) / div) + 1
+    };
+    let e: usize = ((end + 1) / div) + 1;
+    (s..e)
+        .map(|n| nth_delimiter(n as isize))
+        .filter(|&d| d)
+        .count()
+}
+
+//First delimiter has index 1
+fn nth_delimiter(n: isize) -> bool {
+    (((-n & n) << 1) & n) != 0
 }
