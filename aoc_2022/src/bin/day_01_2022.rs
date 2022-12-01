bebbe3c2ffe2165b6cc159c4d4ebf1252e51f0a5
JeanpecheGH <gildasdesnos@hotmail.com>
2022-12-01 23:22:47 +0100

    Add 2022 - Day 01

---
+++ aoc_2022/src/bin/day_01_2022.rs
@@ -0,0 +1,24 @@
+use itertools::Itertools;
+
+fn main() {
+    let s = util::file_as_string("aoc_2022/input/day_01.txt").expect("Cannot open input file");
+    let words: Vec<&str> = s.split("\r\n").collect();
+
+    let elves: Vec<&[&str]> = words.split(|w| w.is_empty()).collect();
+    let calories: Vec<usize> = elves
+        .iter()
+        .map(|&slice| slice.iter().map(|&w| w.parse::<usize>().unwrap()).sum())
+        .collect();
+    let biggest_elf: usize = calories.iter().max().cloned().unwrap();
+
+    println!(
+        "Part1: The elf carrying the more calories has {} calories",
+        biggest_elf
+    );
+
+    let biggest_three_elves: usize = calories.iter().sorted().rev().take(3).sum();
+    println!(
+        "Part2: The three elves carrying the more calories has {} calories",
+        biggest_three_elves
+    );
+}
