60df1a970cd97526d97944a9a63d57d7c40319b8
JeanpecheGH <gildasdesnos@hotmail.com>
2022-12-03 17:31:16 +0100

    Fix Day 14 2016 - Add edge case

--- aoc_2016/src/bin/day_14_2016.rs
+++ aoc_2016/src/bin/day_14_2016.rs
@@ -1,10 +1,11 @@
+use itertools::Itertools;
 use std::collections::HashMap;
 
 fn main() {
     let input: &str = "ahsbgdzn";
 
     let now = std::time::Instant::now();
-    let part1: usize = find_64th_key(input, 1);
+    let part1: usize = find_nth_key(input, 1, 63);
     let elapsed = now.elapsed();
     println!(
         "Part1: The 64th key was found at index {} in {:?}",
@@ -12,7 +13,7 @@ fn main() {
     );
 
     let now = std::time::Instant::now();
-    let part2: usize = find_64th_key(input, 2017);
+    let part2: usize = find_nth_key(input, 2017, 63);
     let elapsed = now.elapsed();
     println!(
         "Part2: The 64th key was found at index {} in {:?}",
@@ -37,57 +38,61 @@ fn hash(word: String, times: usize) -> String {
     format!("{:x}", digest)
 }
 
-fn find_64th_key(input: &str, times_hash: usize) -> usize {
+fn find_nth_key(input: &str, times_hash: usize, n: usize) -> usize {
     let mut keys_index: Vec<usize> = Vec::new();
     let mut candidates: HashMap<usize, char> = HashMap::new();
-    let mut i: usize = 1;
+    let mut i: usize = 0;
+    let mut end_trigger = usize::MAX;
     loop {
         let my_word = format!("{input}{i}");
         let digest_str = hash(my_word, times_hash);
         let chars: Vec<char> = digest_str.chars().collect();
-        match chars.windows(3).find_map(|trio| {
-            if trio[0] == trio[1] && trio[0] == trio[2] {
+        let opt_char: Option<char> = chars.windows(3).find_map(|trio| {
+            if trio.iter().all_equal() {
                 Some(trio[0])
             } else {
                 None
             }
-        }) {
-            None => (),
-            Some(c) => {
-                //Since we have 3 of a kind, search 5 of a kind and promote valid candidates
-                chars
-                    .windows(5)
-                    .filter_map(|quintet| {
-                        if quintet[1..].iter().all(|&c| c == quintet[0]) {
-                            Some(quintet[0])
+        });
+        if let Some(c) = opt_char {
+            //Since we have 3 of a kind, search 5 of a kind and promote valid candidates
+            chars
+                .windows(5)
+                .filter_map(|quintet| {
+                    if quintet.iter().all_equal() {
+                        Some(quintet[0])
+                    } else {
+                        None
+                    }
+                })
+                .for_each(|c| {
+                    candidates.retain(|index, ch| {
+                        if c == *ch {
+                            keys_index.push(*index);
+                            false
                         } else {
-                            None
+                            true
                         }
                     })
-                    .for_each(|c| {
-                        candidates.retain(|index, ch| {
-                            if c == *ch {
-                                keys_index.push(*index);
-                                false
-                            } else {
-                                true
-                            }
-                        })
-                    });
-                candidates.insert(i, c);
-            }
+                });
+            candidates.insert(i, c);
         }
 
         //Remove obsolete candidates
         if i >= 1000 {
             candidates.remove(&(i - 1000));
         }
-        if keys_index.len() >= 64 {
+        // We can only end ~1000 hash after we discovered the first 64 hash
+        // because some valid hash could still be waiting for its 5 char validator
+        if keys_index.len() >= 64 && end_trigger == usize::MAX {
+            end_trigger = i + 1000;
+        }
+        if i >= end_trigger {
             break;
         }
         i += 1;
     }
 
     keys_index.sort();
-    keys_index.into_iter().nth(63).unwrap()
+    keys_index[n]
 }
