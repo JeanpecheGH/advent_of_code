9ee97fac9867ea6831bea36fbd960f155433fcd3
JeanpecheGH <gildasdesnos@hotmail.com>
2022-12-06 19:12:07 +0100

    2022 Day 06 - Factorize

--- aoc_2022/src/bin/day_06_2022.rs
+++ aoc_2022/src/bin/day_06_2022.rs
@@ -1,37 +1,28 @@
 use itertools::Itertools;
 
-const FOUR: usize = 4;
-const FOURTEEN: usize = 14;
-
 fn main() {
     let s = util::file_as_string("aoc_2022/input/day_06.txt").expect("Cannot open input file");
 
     let chars: Vec<char> = s.chars().collect();
-    let idx = chars
-        .windows(FOUR)
-        .enumerate()
-        .find_map(|(idx, four)| {
-            if four.iter().all_unique() {
-                Some(idx + FOUR)
-            } else {
-                None
-            }
-        })
-        .unwrap();
+    let idx = first_marker(&chars, 4);
 
     println!("Part1: The first start-of-packet is after {} chars", idx);
 
-    let idx_2 = chars
-        .windows(FOURTEEN)
+    let idx_2 = first_marker(&chars, 14);
+
+    println!("Part2: The first start-of-message is after {} chars", idx_2);
+}
+
+fn first_marker(chars: &[char], marker_size: usize) -> usize {
+    chars
+        .windows(marker_size)
         .enumerate()
-        .find_map(|(idx, fourteen)| {
-            if fourteen.iter().all_unique() {
-                Some(idx + FOURTEEN)
+        .find_map(|(idx, four)| {
+            if four.iter().all_unique() {
+                Some(idx + marker_size)
             } else {
                 None
             }
         })
-        .unwrap();
-
-    println!("Part2: The first start-of-message is after {} chars", idx_2);
+        .unwrap()
 }
