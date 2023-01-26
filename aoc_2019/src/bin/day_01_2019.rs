0634bac9efeda6e4a5521d8975a50e8cad1e922d
JeanpecheGH <gildasdesnos@hotmail.com>
2023-01-26 19:04:56 +0100

    2019 Day 01 - Cleanup

--- aoc_2019/src/bin/day_01_2019.rs
+++ aoc_2019/src/bin/day_01_2019.rs
@@ -1,21 +1,10 @@
 fn main() {
     let now = std::time::Instant::now();
     let s = util::file_as_string("aoc_2019/input/day_01.txt").expect("Cannot open input file");
-    let sum: usize = s
-        .lines()
-        .map(|l| {
-            let n: usize = l.parse().unwrap();
-            fuel_for_mass(n)
-        })
-        .sum();
+    let numbers: Vec<usize> = s.lines().map(|l| l.parse().unwrap()).collect();
+    let sum: usize = numbers.iter().map(|&n| fuel_for_mass(n)).sum();
     println!("Part1: The sum of all the fuel requirements is {}", sum);
-    let total_sum: usize = s
-        .lines()
-        .map(|l| {
-            let n: usize = l.parse().unwrap();
-            total_fuel(n)
-        })
-        .sum();
+    let total_sum: usize = numbers.iter().map(|&n| total_fuel(n)).sum();
     println!(
         "Part2: The total sum of all the fuel requirements actually is {}",
         total_sum
