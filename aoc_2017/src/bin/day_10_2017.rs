12f9f42877f1a494b0df0756f1185362eb1a8a09
JeanpecheGH <gildasdesnos@hotmail.com>
2024-08-09 20:25:30 +0200

    Extract KnotHash to lib

--- aoc_2017/src/bin/day_10_2017.rs
+++ aoc_2017/src/bin/day_10_2017.rs
@@ -1,103 +1,17 @@
-use itertools::Itertools;
-use nom::character::complete::char;
-use nom::multi::separated_list1;
-use nom::IResult;
-use std::fmt::Write;
-use std::str::FromStr;
-use util::basic_parser::parse_usize;
-
-const SIZE: usize = 256;
-
-#[derive(Debug, Clone)]
-struct KnotHash {
-    lengths: Vec<usize>,
-}
-
-impl KnotHash {
-    fn hash(&self, len: usize) -> usize {
-        let mut v: Vec<usize> = (0..len).collect();
-
-        let mut total_rot: usize = 0;
-
-        for (skip, &l) in self.lengths.iter().enumerate() {
-            let slice: &mut [usize] = &mut v[0..l];
-            slice.reverse();
-            let rot: usize = (l + skip) % len;
-            total_rot = (total_rot + rot) % len;
-            v.rotate_left(rot);
-        }
-
-        //Rotate the vector back to its initial position
-        v.rotate_left(len - total_rot);
-
-        v[0] * v[1]
-    }
-
-    fn dense_hash(&self) -> String {
-        let mut lengths: Vec<u8> = self
-            .lengths
-            .iter()
-            .map(|n| n.to_string())
-            .join(",")
-            .chars()
-            .map(|c| c as u8)
-            .collect();
-        lengths.extend(vec![17, 31, 73, 47, 23]);
-
-        let mut v: Vec<u8> = (0..SIZE).map(|n| n as u8).collect();
-
-        let mut skip: usize = 0;
-        let mut total_rot: usize = 0;
-
-        for _ in 0..64 {
-            for &l in &lengths {
-                let slice: &mut [u8] = &mut v[0..l as usize];
-                slice.reverse();
-                let rot: usize = (l as usize + skip) % SIZE;
-                total_rot = (total_rot + rot) % SIZE;
-                v.rotate_left(rot);
-                skip += 1;
-            }
-        }
-
-        //Rotate the vector back to its initial position
-        v.rotate_left(SIZE - total_rot);
-
-        //v is the sparse hash, densify it
-        v.chunks(16)
-            .map(|c| c.iter().copied().reduce(|a, b| a ^ b).unwrap())
-            .fold(String::new(), |mut output, b| {
-                let _ = write!(output, "{b:02x}");
-                output
-            })
-    }
-}
-
-impl FromStr for KnotHash {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        fn parse_lengths(s: &str) -> IResult<&str, Vec<usize>> {
-            separated_list1(char(','), parse_usize)(s)
-        }
-
-        let lengths: Vec<usize> = s
-            .lines()
-            .next()
-            .map(|l| parse_lengths(l).unwrap().1)
-            .unwrap();
-        Ok(KnotHash { lengths })
-    }
-}
+use util::hashers::KnotHash;
 
 fn main() {
     let now = std::time::Instant::now();
     let s = util::file_as_string("aoc_2017/input/day_10.txt").expect("Cannot open input file");
     let khash: KnotHash = s.parse().unwrap();
-    println!("Part1: The simple hash method gives {}", khash.hash(SIZE));
+    println!(
+        "Part1: The simple hash method gives {}",
+        khash.weak_hash(256)
+    );
+    let khash: KnotHash = KnotHash::new(s.lines().next().unwrap());
     println!(
         "Part2: When using the complete hash method, the result is {}",
-        khash.dense_hash()
+        khash.hash()
     );
     println!("Computing time: {:?}", now.elapsed());
 }
@@ -106,24 +20,34 @@ fn main() {
 mod tests {
     use super::*;
     const EXAMPLE_1: &str = "3,4,1,5";
-    const EXAMPLE_2: &str = "1,2,3";
-    const EXAMPLE_3: &str = "1,2,4";
 
     #[test]
     fn part_1() {
         let khash: KnotHash = EXAMPLE_1.parse().unwrap();
-        assert_eq!(12, khash.hash(5));
+        assert_eq!(12, khash.weak_hash(5));
     }
 
     #[test]
     fn part_2_test_1() {
-        let khash: KnotHash = EXAMPLE_2.parse().unwrap();
-        assert_eq!("3efbe78a8d82f29979031a4aa0b16a9d", khash.dense_hash());
+        let khash: KnotHash = KnotHash::new("");
+        assert_eq!("a2582a3a0e66e6e86e3812dcb672a272", khash.hash());
     }
 
     #[test]
     fn part_2_test_2() {
-        let khash: KnotHash = EXAMPLE_3.parse().unwrap();
-        assert_eq!("63960835bcdc130f0b66d7ff4f6a5a8e", khash.dense_hash());
+        let khash: KnotHash = KnotHash::new("AoC 2017");
+        assert_eq!("33efeb34ea91902bb2f59c9920caa6cd", khash.hash());
+    }
+
+    #[test]
+    fn part_2_test_3() {
+        let khash: KnotHash = KnotHash::new("1,2,3");
+        assert_eq!("3efbe78a8d82f29979031a4aa0b16a9d", khash.hash());
+    }
+
+    #[test]
+    fn part_2_test_4() {
+        let khash: KnotHash = KnotHash::new("1,2,4");
+        assert_eq!("63960835bcdc130f0b66d7ff4f6a5a8e", khash.hash());
     }
 }
---
+++ util/src/hashers.rs
@@ -0,0 +1,97 @@
+use crate::basic_parser::parse_usize;
+use nom::character::complete::char;
+use nom::multi::separated_list1;
+use nom::IResult;
+use std::fmt::Write;
+use std::str::FromStr;
+
+#[derive(Debug, Clone)]
+pub struct KnotHash {
+    lengths: Vec<u8>,
+}
+
+impl KnotHash {
+    pub fn from_usize_list(l: &[usize]) -> KnotHash {
+        KnotHash {
+            lengths: l.iter().map(|n| *n as u8).collect(),
+        }
+    }
+    pub fn new(s: &str) -> KnotHash {
+        let lengths: Vec<u8> = s.chars().map(|c| c as u8).collect();
+        KnotHash { lengths }
+    }
+
+    pub fn weak_hash(&self, len: usize) -> usize {
+        let mut v: Vec<usize> = (0..len).collect();
+
+        let mut total_rot: usize = 0;
+
+        for (skip, &l) in self.lengths.iter().enumerate() {
+            let slice: &mut [usize] = &mut v[0..l as usize];
+            slice.reverse();
+            let rot: usize = (l as usize + skip) % len;
+            total_rot = (total_rot + rot) % len;
+            v.rotate_left(rot);
+        }
+
+        //Rotate the vector back to its initial position
+        v.rotate_left(len - total_rot);
+
+        v[0] * v[1]
+    }
+
+    pub fn hash_vec(&self) -> Vec<u8> {
+        let mut lengths: Vec<u8> = self.lengths.clone();
+        lengths.extend(vec![17, 31, 73, 47, 23]);
+
+        let mut v: Vec<u8> = (0..256).map(|n| n as u8).collect();
+
+        let mut skip: usize = 0;
+        let mut total_rot: usize = 0;
+
+        for _ in 0..64 {
+            for &l in &lengths {
+                let slice: &mut [u8] = &mut v[0..l as usize];
+                slice.reverse();
+                let rot: usize = (l as usize + skip) % 256;
+                total_rot = (total_rot + rot) % 256;
+                v.rotate_left(rot);
+                skip += 1;
+            }
+        }
+
+        //Rotate the vector back to its initial position
+        v.rotate_right(total_rot);
+
+        //v is the sparse hash, densify it
+        v.chunks(16)
+            .map(|c| c.iter().copied().reduce(|a, b| a ^ b).unwrap())
+            .collect()
+    }
+
+    pub fn hash(&self) -> String {
+        self.hash_vec()
+            .into_iter()
+            .fold(String::new(), |mut output, b| {
+                let _ = write!(output, "{b:02x}");
+                output
+            })
+    }
+}
+
+impl FromStr for KnotHash {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        fn parse_lengths(s: &str) -> IResult<&str, Vec<usize>> {
+            separated_list1(char(','), parse_usize)(s)
+        }
+
+        let lengths: Vec<usize> = s
+            .lines()
+            .next()
+            .map(|l| parse_lengths(l).unwrap().1)
+            .unwrap();
+        Ok(KnotHash::from_usize_list(&lengths))
+    }
+}
